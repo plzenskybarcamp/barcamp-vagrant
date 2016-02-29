02dbe685b5725cd50ff2b9b059984daf0912f9bf
Jakub Bouček <pan@jakubboucek.cz>
2016-02-29 05:05:51 +0100

    Sync rename project by new repository: https://github.com/plzenskybarcamp/plzenskybarcamp-web

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,8 @@ apt-get update
 apt-get install -y mongodb-org php5 php5-dev libcurl4-openssl-dev
 pecl install mongodb
 
-cat ./apache-barcamp.conf > /etc/apache2/sites-available/barcamp.conf
-a2ensite barcamp
+cat ./apache-plzenskybarcamp.conf > /etc/apache2/sites-available/plzenskybarcamp.conf
+a2ensite plzenskybarcamp
 echo "extension=mongodb.so" > /etc/php5/mods-available/mongodb.ini
 php5enmod mongodb
 service apache2 restart
